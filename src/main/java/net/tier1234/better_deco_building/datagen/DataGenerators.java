ac8dd6740ea89fca7fad4b7e22165908f843e93f
TearPelato <165576102+TearPelato@users.noreply.github.com>
2026-02-22 21:39:30 +0100

    Add Datagen register

---
+++ src/main/java/net/tier1234/better_deco_building/datagen/DataGenerators.java
@@ -0,0 +1,26 @@
+package net.tier1234.better_deco_building.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+import net.tier1234.better_deco_building.Constants;
+
+import java.util.concurrent.CompletableFuture;
+
+@EventBusSubscriber(modid = Constants.MOD_ID, value = Dist.CLIENT)
+public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+
+    }
+}
